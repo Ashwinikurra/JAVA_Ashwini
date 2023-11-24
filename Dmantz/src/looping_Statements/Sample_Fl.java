e45c9f91436482cd01ac2b5793caee964eb08d51
Ashwinikurra <ashwinikurra136@gmail.com>
2023-11-24 17:10:57 +0530

    sample programs

---
+++ Dmantz/src/looping_Statements/Sample_Fl.java
@@ -0,0 +1,19 @@
+//A PROGRAM TO PRINT USERNAME AND USERDEFINED TIMES 
+
+
+package looping_Statements;
+import java.util.Scanner;
+public class Sample_Fl 
+{
+  public static void main(String[]args)
+  {
+
+	  String name="ashwini";
+	String intial="kurra";
+	 System.out.println("how many times you wantto print your name  ");
+	     for(int i=1;i<=100;i++)
+	     {
+		     System.out.println(i+name+" "+intial);  
+	     }
+   }
+}
