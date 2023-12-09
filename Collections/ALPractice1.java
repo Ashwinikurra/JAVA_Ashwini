1d73bacb44dd260845d1195a71a9064be1ad7733
Ashwinikurra <ashwinikurra136@gmail.com>
2023-12-09 15:11:46 +0530

    programs

--- Collections/ALPractice1.java
+++ Collections/ALPractice1.java
@@ -1,5 +1,31 @@
 package com.corejava.Collections;
-
-public class ALPractice1 {
-
-}
+import java.util.*;
+import java.util.ArrayList;
+public class ALPractice1 
+{
+	public static void main(String[] args)
+	{
+		ArrayList<Integer> aL= new ArrayList<Integer>();
+		aL.add(12);
+		aL.add(20);
+		aL.add(28);
+		aL.add(null);
+		aL.add(-23);
+		aL.add(0);
+		aL.add(20);
+		System.out.println(aL);
+		aL.remove(3);
+		aL.remove(new Integer(0));
+		System.out.println(aL);
+		System.out.println(aL.get(3));
+	    System.out.println(aL);
+	    aL.set(3,8);
+	    System.out.println(aL.size());
+	    System.out.println(aL.isEmpty());
+	    System.out.println(aL.contains(new Integer(12)));
+	    System.out.println(aL);
+	    aL.clear();
+	    System.out.println(aL);
+		
+		}
+  }
